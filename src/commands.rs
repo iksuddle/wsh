6360a674b653b82d24b9964f92e997c36e8df506
iksuddle <iksuddle@gmail.com>
2025-06-14 02:26:19 -0400

    move cmd to commands.rs

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,58 @@
 use std::env;
 
+#[derive(Debug)]
+pub enum Cmd {
+    Exit,
+    Cd(Vec<String>),
+    Pwd(Vec<String>),
+    SetVar(String, String),
+    GetVar(Vec<String>),
+    ListVars,
+    External(Vec<String>),
+    Error(String),
+}
+
+impl Cmd {
+    pub fn from(cmd: Vec<String>) -> Cmd {
+        match cmd.first().unwrap().as_str() {
+            "exit" => Cmd::Exit,
+            "cd" => Cmd::Cd(cmd),
+            "pwd" => Cmd::Pwd(cmd),
+            "lsv" => Cmd::ListVars,
+            "get" => Cmd::GetVar(cmd),
+            _ => Cmd::External(cmd),
+        }
+    }
+
+    pub fn build_piped_commands<'a>(input: impl Iterator<Item = &'a str>) -> Vec<Cmd> {
+        let mut cmds = vec![];
+        let mut curr_cmd = vec![];
+
+        for tok in input {
+            if tok == "|" {
+                if !curr_cmd.is_empty() {
+                    cmds.push(Cmd::from(curr_cmd.clone()));
+                    curr_cmd.clear();
+                } else {
+                    // error -> | |
+                    cmds.push(Cmd::Error("syntax error: | |".to_owned()));
+                    curr_cmd.clear();
+                    break;
+                }
+            } else {
+                curr_cmd.push(tok.to_owned());
+            }
+        }
+
+        // last one
+        if !curr_cmd.is_empty() {
+            cmds.push(Cmd::from(curr_cmd));
+        }
+
+        cmds
+    }
+}
+
 pub fn cd(args: &[String]) {
     match args.len() {
         1 => {
--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,8 @@ use std::{
 use nix::sys::signal::{SigSet, Signal};
 
 mod commands;
+use commands::Cmd;
 
-#[derive(Debug)]
-enum Cmd {
-    Exit,
-    Cd(Vec<String>),
-    Pwd(Vec<String>),
-    SetVar(String, String),
-    GetVar(Vec<String>),
-    ListVars,
-    External(Vec<String>),
-    Error(String),
-}
 pub struct Shell {
     prompt: String,
     env_vars: HashMap<String, String>,
@@ -174,47 +164,6 @@ impl Shell {
         for (k, v) in &self.env_vars {
             println!("{k}: {v}");
         }
-    }
-}
-
-impl Cmd {
-    fn from(cmd: Vec<String>) -> Cmd {
-        match cmd.first().unwrap().as_str() {
-            "exit" => Cmd::Exit,
-            "cd" => Cmd::Cd(cmd),
-            "pwd" => Cmd::Pwd(cmd),
-            "lsv" => Cmd::ListVars,
-            "get" => Cmd::GetVar(cmd),
-            _ => Cmd::External(cmd),
-        }
-    }
-
-    fn build_piped_commands<'a>(input: impl Iterator<Item = &'a str>) -> Vec<Cmd> {
-        let mut cmds = vec![];
-        let mut curr_cmd = vec![];
-
-        for tok in input {
-            if tok == "|" {
-                if !curr_cmd.is_empty() {
-                    cmds.push(Cmd::from(curr_cmd.clone()));
-                    curr_cmd.clear();
-                } else {
-                    // error -> | |
-                    cmds.push(Cmd::Error("syntax error: | |".to_owned()));
-                    curr_cmd.clear();
-                    break;
-                }
-            } else {
-                curr_cmd.push(tok.to_owned());
-            }
-        }
-
-        // last one
-        if !curr_cmd.is_empty() {
-            cmds.push(Cmd::from(curr_cmd));
-        }
-
-        cmds
     }
 }
 
